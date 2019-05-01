302e1bc06dd65d762d82c011c44a5dea47872a9a
DeEpicOfGames <dacjan3@onet.pl>
2019-05-01 12:33:09 +0200

    Usunieto powiadomienie, dodano odliczanie na stronie

--- src/byebyeshinden.js
+++ src/byebyeshinden.js
@@ -1,8 +1,11 @@
+var data;
+var result;
+
 function getReq(theUrl, callback=null)
 {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        if (callback != null && xmlHttp.readyState == 4 && xmlHttp.status == 200)
             callback(xmlHttp.responseText);
     };
     xmlHttp.open("GET", theUrl, true);
@@ -11,30 +14,39 @@ function getReq(theUrl, callback=null)
 
 function addEventListeners(source) {
 	var pattern = /_Storage\.basic =  \'.*\'/;
-	var result = pattern.exec(source)[0].substr(19).slice(0, -1);
+	result = pattern.exec(source)[0].substr(19).slice(0, -1);
 	
 	var elements = document.getElementsByClassName("ep-buttons");
 	var i;
 
 	for (i = 1; i < elements.length; i++) {
-		// remove event liseners
+		// remove event listeners
 		clone = elements[i].getElementsByTagName("a")[0].cloneNode(true);
 		elements[i].replaceChild(clone, elements[i].getElementsByTagName("a")[0]);
 
 		elements[i].getElementsByTagName("a")[0].addEventListener("click", function(event){
-			var data = JSON.parse(event.target.getAttribute("data-episode"));
-			alert("Poczekaj 5 sekund a odtwarzacz się pojawi");
+			data = JSON.parse(event.target.getAttribute("data-episode"));
 			getReq("https://api4.shinden.pl/xhr/" + data["online_id"] + "/player_load?auth=" + result);
-			setTimeout(function () {
-				getReq("https://api4.shinden.pl/xhr/" + data["online_id"] + "/player_show?auth=" + result + "&width=765", replace);
-			}, 5000);
+			countdown(5);
 		});
 	};
 };
 
-function replace(data) {
+function countdown(time)
+{
+	if (time <= 0) {
+		getReq("https://api4.shinden.pl/xhr/" + data["online_id"] + "/player_show?auth=" + result + "&width=765", replace);
+	} else {
+		document.getElementsByClassName("player-online box")[0].innerHTML = "<h2> Odliczanie: " + time.toString() + "</h2>";
+		setTimeout(function() {
+			countdown(time-1);
+		}, 1000);
+	};
+};
+
+function replace(player) {
 	// put it to html code
-	document.getElementsByClassName("player-online box")[0].innerHTML = data;
+	document.getElementsByClassName("player-online box")[0].innerHTML = player;
 };
 
 getReq(window.location.href, addEventListeners);
